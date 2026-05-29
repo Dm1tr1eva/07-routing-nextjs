34c7ddb141c79e7ad5f5214a77b843e591e76625
Dm1tr1eva <114660725+Dm1tr1eva@users.noreply.github.com>
2026-05-29 16:55:04 +0200

    Update page.tsx

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,17 +1,3 @@
-// app/profile/[id]/page.tsx
-
-// import { notFound } from "next/navigation";
-
-// export default async function ProfilePage({
-//   params,
-// }: {
-//   params: { id: string };
-// }) {
-//   const user = await getUser(params.id);
-
-//   if (!user) {
-//     notFound(); // Показує /profile/not-found.tsx
-//   }
-
-//   return <div>{user.name}</div>;
-// }
+export default async function ProfilePage() {
+  return <div>Profile</div>;
+}
